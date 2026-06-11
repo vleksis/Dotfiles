8764fe7c87d8dc998d35b4a3c3a42f7f5872ded3
vleksis <vleksis@yandex.ru>
2026-06-11 20:26:12 +0200

    Add editor language tooling

--- home/vleksis/packages.nix
+++ home/vleksis/packages.nix
@@ -24,12 +24,72 @@
     eza
     mcfly
 
-    rustup
+    python3
+    pyright
+    ruff
+    black
+    isort
+    uv
+
+    rustc
+    cargo
+    rustfmt
+    clippy
+    rust-analyzer
     cargo-watch
     cargo-edit
     cargo-nextest
+
+    go
+    gopls
+    gotools
+    gofumpt
+    golangci-lint
+    delve
+
     gcc
+    clang-tools
+    cmake
+    gnumake
+    pkg-config
     openssl
+
+    nixd
+    nil
+    nixfmt
+    statix
+    deadnix
+
+    bash-language-server
+    shellcheck
+    shfmt
+    fish-lsp
+
+    vim-language-server
+
+    taplo
+    yaml-language-server
+    yamlfmt
+    yamllint
+    vscode-langservers-extracted
+    marksman
+
+    dockerfile-language-server
+    docker-compose-language-service
+    hadolint
+
+    sqls
+    sqlfluff
+
+    actionlint
+    gh
+
+    typescript-language-server
+    prettier
+    biome
+
+    lua-language-server
+    stylua
 
     telegram-desktop
 
