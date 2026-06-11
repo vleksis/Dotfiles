6bae5934763c674f2c477ac6c2e7b4d696019121
vleksis <vleksis@yandex.ru>
2026-06-11 20:52:02 +0200

    Add second OS bootloader

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -6,6 +6,15 @@
     efiSupport = true;
     device = "nodev";
     useOSProber = true;
+
+    extraEntries = ''
+      menuentry "CachyOS (Limine)" {
+        insmod part_gpt
+        insmod fat
+        search --no-floppy --fs-uuid --set=esp F585-4E9E
+        chainloader ($esp)/EFI/limine/limine_x64.efi
+      }
+    '';
   };
 
   boot.loader.efi = {
