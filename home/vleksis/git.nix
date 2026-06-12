3b180bfa535f7caf82f73b96463607b5b4e85b6e
vleksis <vleksis@yandex.ru>
2026-06-12 14:28:39 +0200

    Update git settings

--- home/vleksis/git.nix
+++ home/vleksis/git.nix
@@ -1,14 +1,44 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.git = {
     enable = true;
 
     settings = {
-      user = {
-        name = "vleksis";
-        email = "vleksis@yandex.ru";
+      user.name = "vleksis";
+      user.email = "vleksis@yandex.ru";
+
+      init.defaultBranch = "main";
+      trim.bases = "develop,master,main";
+
+      url = {
+        "ssh://git@github.com/vleksis" = {
+          insteadOf = "https://github.com/vleksis";
+        };
+
+        "ssh://git@gitlab.com/vleksis" = {
+          insteadOf = "https://gitlab.com/vleksis";
+        };
       };
+
+      alias = {
+        co = "checkout";
+        dc = "diff --cached";
+        amend = "commit --amend";
+      };
+    };
+  };
+
+  programs.gh.enable = true;
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      line-numbers = true;
+      navigate = true;
+      side-by-side = true;
+      true-color = "always";
     };
   };
 }
