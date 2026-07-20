9107b43d5c8f0d78714795476866702c3308352a
vleksis <vleksis@yandex.ru>
2026-07-20 19:19:46 +0300

    Add mac ssh public key

--- modules/homelab/ssh.nix
+++ modules/homelab/ssh.nix
@@ -14,6 +14,6 @@ _:
   };
 
   users.users.vleksis.openssh.authorizedKeys.keys = [
-    # macbook
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+NQ9bFqQLcMHW50/Evq4nPc0HdEwjacVt3Gp3rIsrT vleksis@Maltsevs-MacBook-Air.local"
   ];
 }
