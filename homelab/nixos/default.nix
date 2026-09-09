342f1e568dc31205085f8c510821bdf469b2cad5
vleksis <vleksis@yandex.ru>
2026-09-09 15:06:30 +0300

    Remove temporary testing secret

--- homelab/nixos/default.nix
+++ homelab/nixos/default.nix
@@ -7,7 +7,5 @@ _:
     useSystemdActivation = true;
     defaultSopsFile = ../../secrets/homelab.yaml;
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-
-    secrets.test-secret = { };
   };
 }
